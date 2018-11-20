ae4977725886d3f23812ad83658c020a8b213518
Delfs <made@html24.net>
2018-11-20 18:30:37 +0100

    removed unused code

--- src/main/resources/static/palindrome.js
+++ src/main/resources/static/palindrome.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
 
             let pal = JSON.parse(palindrome.body);
 
-
             $("#palindromes tr:last")
                 .after("" +
                     "<tr>" +
@@ -21,25 +20,8 @@ $(document).ready(function () {
                         "<td>" + pal.timestamp + "</td>" +
                         "<td>" + pal.longest_palindrome_size + "</td>" +
                     "</tr>" );
-
-
-            // var listItem = document.createElement("li");
-            // listItem.className = "list-group-item";
-            // listItem.textContent = "Text: " + pal.content + " created at: " + pal.timestamp;
-
-            // ul.appendChild(listItem);
-
-
-            // $("#content").html("Content: " + pal.content);
-            // $("#timestamp").html("Timestamp: " + pal.timestamp);
-
         })
-
     },onError())
-
-    // $(function () {
-    //     $('[data-toggle="tooltip"]').tooltip()
-    // })
 });
 
 
